d27b5ecbd7500c63a166f4b37b608aca72b22ab1
Alexander Pekhterev <alexander.pekhterev@comyan.com>
2018-01-22 16:34:58 +0700

    Initial Commit

---
+++ MyTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestProject
+//
+//  Created by Alexandr Pekhterev on 22/01/2018.
+//  Copyright © 2018 Alexandr Pekhterev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTestProject
+//
+//  Created by Alexandr Pekhterev on 22/01/2018.
+//  Copyright © 2018 Alexandr Pekhterev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
